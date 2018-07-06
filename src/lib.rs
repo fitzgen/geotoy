de51d8528ceb382d150af9cb904dbcf1dccefa4d
Johannes Hoff <johshoff@gmail.com>
2018-07-05 20:41:56 -0700

    Update shader to lib from file
    Missed this in the merge

--- src/lib.rs
+++ src/lib.rs
@@ -222,12 +222,13 @@ pub const VERTEX_SHADER: &str = "
 
                 uniform float a;
                 uniform float b;
+                uniform vec2 offset;
 
                 void main() {
-                    float multiplier = kind == uint(0) ? b : a;
+                    float multiplier = kind == uint(2) ? 0 : (kind == uint(0) ? b : a);
 
-                    vec2 p = vec2(x, y);
-                    vec2 v = attractor - p;
+                    vec2 p = vec2(x, y) + offset;
+                    vec2 v = attractor + offset - p;
                     gl_Position = vec4(p + multiplier * v, 0.0, 1.0);
                 }
 ";
